d6fd21e14b7decfba16c0e9f50400486247b3852
nekkan <nxkkdev@gmail.com>
2021-02-12 16:11:47 +0000

    Auto-save #9

---
+++ server/plugins/PlaceholderAPI/javascripts/dimension.js
@@ -0,0 +1,15 @@
+var dimension = args[0];
+
+function get_dimension() {
+    switch(dimension) {
+        case 'world':
+            return '&e♦ Overworld';
+        case 'world_the_nether':
+            return '&4☠ Nether';
+        case 'world_the_end':
+            return '&d۞ Dimensão do Fim';
+    }
+    return '&c☠ Unknown'
+}
+
+get_dimension();
